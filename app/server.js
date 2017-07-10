3cf00b2c12373005aa26b37d9bf8233141f36b84
Vivek Ji <vivekjib@gmail.com>
2017-07-10 19:30:36 +0530

    Build whole new UI

--- app/server.js
+++ app/server.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var app = express();
+var root = process.cwd();
+var favicon = require('serve-favicon');
+var path = require('path');
+
+
+app.use(express.static('html'));
+app.use(favicon(path.join(__dirname, 'html', 'images', 'favicon.ico')));
+
 
 //your routes here
-app.get('/', function (req, res) {
-    res.sendFile("/index.html");
+/*app.get('/login', function (req, res) {
+    res.sendFile('html/login/index.html', {root});
 });
+
+app.get('/signup', function (req, res) {
+    res.sendFile('html/login/index.html', {root});
+});
+*/
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
