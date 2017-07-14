aad62d91447a7e1931233c57e412a2c8e722b632
Vivek Ji <vivekjib@gmail.com>
2017-07-14 22:21:58 +0530

    Added working login and sign up

---
+++ app/html/Carousel Template for Bootstrap_files/auth.js
@@ -0,0 +1,211 @@
+$('[data-toggle="tooltip"]').tooltip();
+
+//Auth
+var auth_url = "http://auth.c100.hasura.me";
+var data_url = "http://data.c100.hasura.me";
+//var auth_url = "http://auth.vivekjib.hasura.me";
+//var data_url = "http://data.vivekjib.hasura.me";
+(Cookies.get('id') == undefined) ? $('#out').css("display", "none"): $('#out').css("display", "block");
+(Cookies.get('id') == undefined) ? $('#in').css("display", "block"): $('#in').css("display", "none");
+
+
+
+//login
+$('#login_form').on('click', function () {
+    //alert("clicked");
+
+    $(this).attr("disabled", true);
+    var $usern = $('#username');
+    var $pass = $('#password');
+    var $user = {
+        username: $usern.val(),
+        password: $pass.val()
+    };
+
+
+    $.ajax({
+        method: 'POST',
+
+        url: auth_url + '/login',
+        xhrFields: {
+            withCredentials: true
+        },
+
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify($user)
+    }).done(function (data) {
+                
+        user_id = data.hasura_id;
+        Cookies.set('id', user_id, {
+            expires: 7
+        });
+        window.location = "../after-login/index.html";
+        toastr["success"]("Login successful")
+        toastr.options = {
+            "closeButton": true,
+        }
+    }).fail(function (error) {
+
+        $('#login_form').attr("disabled", false);
+        
+        var mes = (JSON.parse(error.responseText).message).toString();
+        toastr["error"](mes)
+        toastr.options = {
+            "closeButton": true,
+        }
+    })
+})
+//signup
+
+$('#signup_form').on('click touchstart', function () {
+    $(this).attr("disabled", true);
+    var $usern = $('#username');
+    var $pass = $('#password');
+    var $email = $('#email');
+    var $user = {
+        username: $usern.val(),
+        password: $pass.val(),
+        email: $email.val()
+    };
+
+    $.ajax({
+        method: 'POST',
+        url: auth_url + '/signup',
+        xhrFields: {
+            withCredentials: true
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify($user)
+    }).done(function (data) {
+        toastr["success"]("Signup successful")
+        toastr.options = {
+            "closeButton": true,
+        }
+        console.log(data);
+        user_id = data.hasura_id;
+        Cookies.set('id', user_id, {
+            expires: 7
+        });
+        var $profile = {
+            type: 'insert',
+            args: {
+                table: 'profile',
+                objects: [{
+                    'name': $usern.val(),
+                    'user_id': user_id
+                        }]
+            }
+        };
+        $.ajax({
+            method: 'POST',
+
+            url: data_url + '/v1/query',
+            xhrFields: {
+                withCredentials: true
+            },
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify($profile)
+        }).done(function (data) {
+            //console.log(data);
+
+
+            window.location = "login.html";
+        }).fail(function (error) {
+            $('#signup_form').attr("disabled", false);
+            console.log(error);
+            //alert(JSON.parse(error.responseText).message);
+            
+        })
+        window.location = "login.html";
+    }).fail(function (error) {
+        $('#signup_form').attr("disabled", false);
+         var mes = (JSON.parse(error.responseText).message).toString();
+        toastr["error"](mes)
+        toastr.options = {
+            "closeButton": true,
+        }
+        console.log(error);
+       // alert(JSON.parse(error.responseText).message);
+    })
+})
+
+
+//logout
+$('#logout').on('click touchstart', function () {
+    $(this).attr("disabled", true);
+    $.ajax({
+        method: 'POST',
+        url: auth_url + '/user/logout',
+        xhrFields: {
+            withCredentials: true
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        }
+
+    }).done(function (data) {
+        console.log(data);
+        Cookies.remove('id');
+        toastr["success"]("Logged out")
+        toastr.options = {
+            "closeButton": true,
+        }
+        window.location = "/";
+    }).fail(function (error) {
+        $('#logout').attr("disabled", false);
+        console.log(error);
+    })
+})
+
+
+//Insert a new resource into the table by the user
+$('#add_resource').on('click', function () {
+    $(this).attr("disabled", true);
+    var certificate = ($('[name="cert"]:checked').val() == 1) ? true : false;
+    var price = ($('[name="paid"]:checked').val() == 1) ? true : false;
+
+    var $info = {
+        type: 'insert',
+        args: {
+            table: 'resource',
+            objects: [{
+                resource_url: $('[name="r_url"]').val(),
+                name: $('[name="name"]').val(),
+                topic: $('[name="topic"]').val(),
+                user_id: Cookies.get('id'),
+                type: $('[name="type"]:checked').val(),
+                certificate: certificate,
+                cost: price,
+                description: $('[name="desc"]').val(),
+                }]
+        }
+    }
+    //console.log(JSON.stringify($info));
+    $.ajax({
+        method: 'POST',
+        url: data_url + '/v1/query',
+        xhrFields: {
+            withCredentials: true
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify($info)
+    }).done(function (data) {
+        console.log(data);
+
+
+        window.location = "/";
+        
+    }).fail(function (error) {
+        $('#signup_form').attr("disabled", false);
+        console.log(error);
+        alert(JSON.parse(error.responseText).error);
+    })
+})
---
+++ app/html/Carousel Template for Bootstrap_files/data.js
@@ -0,0 +1,170 @@
+//upvote
+$("[id^=tu-]").on('click', function () {
+    var str = $(this).attr("id");
+    var r_url = str.slice(3);
+    var $vote = {
+        "type": "insert",
+        "args": {
+            "table": "votes",
+            "objects": [
+                {
+                    "user_id": Cookies.get('id'),
+                    "resource": r_url,
+                    "value": 1
+			}
+			]
+        }
+    }
+    $.ajax({
+        method: 'POST',
+        url: data_url + '/v1/query',
+        xhrFields: {
+            withCredentials: true
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify($vote)
+    }).done(function (data) {
+        console.log(data);
+        toastr["success"]("Your vote is recorded")
+            toastr.options = {
+                "closeButton": true,}
+        update(r_url);
+
+    }).fail(function (error) {
+        if (error.status == 400) {
+            delete_vote(r_url);
+        }
+        console.log(error.status);
+        //alert(JSON.parse(error.responseText).message);
+    })
+
+})
+//down vote
+$("[id^=td-]").on('click', function () {
+    var str = $(this).attr("id");
+    var r_url = str.slice(3);
+    var $vote = {
+        "type": "insert",
+        "args": {
+            "table": "votes",
+            "objects": [
+                {
+                    "user_id": Cookies.get('id'),
+                    "resource": r_url,
+                    "value": -1
+			}
+			]
+        }
+    }
+    $.ajax({
+        method: 'POST',
+        //url: 'http://data.priyesh18.hasura.me/v1/query',
+        url: data_url + '/v1/query',
+        xhrFields: {
+            withCredentials: true
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify($vote)
+    }).done(function (data) {
+        console.log(data);
+        toastr["success"]("Your vote is recorded")
+            toastr.options = {
+                "closeButton": true,}
+        update(r_url);
+
+        //window.location = "/";
+    }).fail(function (error) {
+        if (error.status == 400) {
+            delete_vote(r_url);
+        }
+        console.log(error);
+        //alert(JSON.parse(error.responseText).message);
+    })
+
+})
+
+
+//delete vote function
+function delete_vote(rurl) {
+    var $delete_original = {
+        type: 'delete',
+        args: {
+            table: 'votes',
+            where: {
+                user_id: Cookies.get('id'),
+                resource: rurl
+            }
+
+        }
+
+    }
+    $.ajax({
+        method: 'POST',
+        url: data_url + '/v1/query',
+        xhrFields: {
+            withCredentials: true
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify($delete_original)
+    }).done(function (data) {
+        console.log(data);
+        toastr["error"]("Your vote was removed")
+            toastr.options = {
+                "closeButton": true,}
+        update(rurl);
+
+        //window.location = "/";
+    }).fail(function (error) {
+            toastr["warning"]("Please login to vote")
+            toastr.options = {
+                "closeButton": true,}
+                console.log(error);
+                //alert(JSON.parse(error.responseText).message);
+            })
+    }
+
+    //update the total votes count
+    function update(uurl) {
+        //var id = 'tv-'+url;
+        var $update = {
+            type: 'select',
+            args: {
+                table: 'resource_info',
+                columns: ['total_votes'],
+                where: {
+                    resource_url: uurl
+                }
+            }
+        }
+        $.ajax({
+            method: 'POST',
+            //url: 'http://data.priyesh18.hasura.me/v1/query',
+            url: data_url + '/v1/query',
+            xhrFields: {
+                withCredentials: true
+            },
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify($update)
+        }).done(function (data) {
+            var x = JSON.stringify(data[0].total_votes); //data is an array with 1 element
+            if (x == 'null') {
+                x = '0';
+            }
+            document.getElementById('tv-' + uurl).innerHTML = x;
+            //window.location = "/";
+        }).fail(function (error) {
+
+            console.log(error);
+            //alert(JSON.parse(error.responseText).message);
+        })
+        //console.log($id);
+
+    }
